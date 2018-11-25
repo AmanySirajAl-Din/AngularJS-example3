45fcc4d9cc820a960d34a8a4ac96c68764feda2d
Amany Siraj <amanysirajaldin@gmail.com>
2018-11-25 18:33:35 +0200

    2nd Aproach to protect the dependency injection from Minification.

--- app.js
+++ app.js
@@ -1,37 +1,33 @@
 (function(){
-'use strict';
+    'use strict';
 
-angular.module('DIApp', [])
+    angular.module('DIApp', [])
 
-// To protect the dependency injection from Minification
-// first aproach is
-// specify an array giving it the arguments of the controller function and the last element will be the controller function itself
-// so no matter the minification does to the arguments it's already been specified
-// the array elements is saved from minification cause they are string literal that will never get mimified cause it's real data
-    
-.controller('DIController', ['$scope', '$filter', '$injector', DIController]);
+    // To protect the dependency injection from Minification
+    // first aproach is
+    // specify an array giving it the arguments of the controller function and the last element will be the controller function itself
+    // so no matter the minification does to the arguments it's already been specified
+    // the array elements is saved from minification cause they are string literal that will never get mimified cause it's real data
 
-// $filter service lets us create filtering functions
-//that used for formatting the data that eventually gets displayed to the user
+    /*.controller('DIController', ['$scope', '$filter', '$injector', DIController]);*/
     
-// $injector service is the service which do all the magic
-// of finding where to inject which services 
-function DIController ($scope,
-                        $filter, 
-                        $injector){
-    $scope.name = "";
-    
-    // function will run when the textbox loses focus
-    // this function will convert the text in the textbox to uppercase
-    $scope.upper = function(){
-        var upCase = $filter('uppercase');
-        $scope.name = upCase($scope.name);
-    };
-    
-    console.log($injector.annotate(DIController));
-    // this will return an array of the arguments of the DIController
-}
-    
+    // 2nd aproach to do an inline function
+    .controller('DIController', ['$scope', '$filter', '$injector', function($scope,
+                $filter, 
+                $injector){
+       $scope.name = "";
 
-    
+        // function will run when the textbox loses focus
+        // this function will convert the text in the textbox to uppercase
+        $scope.upper = function(){
+            var upCase = $filter('uppercase');
+            $scope.name = upCase($scope.name);
+        }; 
+    }]);
+
+    // $filter service lets us create filtering functions
+    //that used for formatting the data that eventually gets displayed to the user
+
+    // $injector service is the service which do all the magic
+    // of finding where to inject which services 
 })();
