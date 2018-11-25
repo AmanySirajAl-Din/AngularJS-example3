29f5d687060b500810b9c7b09f1147ee0c0bec2a
Amany Siraj <amanysirajaldin@gmail.com>
2018-11-25 17:50:21 +0200

    Creating upper fun. using 'uppercase filter.

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ angular.module('DIApp', [])
 //that used for formatting the data that eventually gets displayed to the user
 function DIController ($scope, $filter){
     $scope.name = "Amany";
+    
+    // functions will run when the textbox loses focus
+    // this function will convert the text in the textbox to uppercase
+    $scope.upper = function(){
+        var upCase = $filter('uppercase');
+        $scope.name = upCase($scope.name);
+    };
 }
     
 })();
