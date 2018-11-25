d005017854f4314396a949024cfd0939878b9643
Amany Siraj <amanysirajaldin@gmail.com>
2018-11-25 17:51:27 +0200

    Use upper() in the view.

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ angular.module('DIApp', [])
 // $filter service lets us create filtering functions
 //that used for formatting the data that eventually gets displayed to the user
 function DIController ($scope, $filter){
-    $scope.name = "Amany";
+    $scope.name = "";
     
     // functions will run when the textbox loses focus
     // this function will convert the text in the textbox to uppercase
