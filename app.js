5fc112f04388dd0febc3380db3995491f5d832dd
Amany Siraj <amanysirajaldin@gmail.com>
2018-11-25 17:40:09 +0200

    Add  service.

--- app.js
+++ app.js
@@ -4,8 +4,10 @@
 angular.module('DIApp', [])
 
 .controller('DIController', DIController);
-    
-function DIController ($scope){
+
+// $filter service lets us create filtering functions
+//that used for formatting the data that eventually gets displayed to the user
+function DIController ($scope, $filter){
     $scope.name = "Amany";
 }
     
